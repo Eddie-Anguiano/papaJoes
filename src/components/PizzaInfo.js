85b16aee0e63cb70e842ce4fe867fb565be7bd2c
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-11-07 19:32:34 -0800

    add Prices component

--- src/components/PizzaInfo.js
+++ src/components/PizzaInfo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Sizes from "./Sizes";
+import Prices from "./Prices";
 
 export default function PizzaInfo() {
   return (
     <div className="pizzaInfo">
       <div className="pizzaInfo-heading  menu-heading">Pizza</div>
       <Sizes />
+      <Prices />
     </div>
   );
 }
---
+++ src/components/Prices.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default function Prices() {
+  return (
+    <table className="prices">
+      <tr className="prices-row">
+        <th className="prices-heading"></th>
+        <th className="prices-heading">Indv</th>
+        <th className="prices-heading">Small</th>
+        <th className="prices-heading">Medium</th>
+        <th className="prices-heading">Large</th>
+      </tr>
+      <tr className="prices-row">
+        <th className="prices-heading">Cheese</th>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+      </tr>
+      <tr className="prices-row">
+        <th className="prices-heading">Cheese</th>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+      </tr>
+      <tr className="prices-row">
+        <th className="prices-heading">Cheese</th>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+      </tr>
+      <tr className="prices-row">
+        <th className="prices-heading">Cheese</th>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+      </tr>
+      <tr className="prices-row">
+        <th className="prices-heading">Cheese</th>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+      </tr>
+      <tr className="prices-row">
+        <th className="prices-heading">Add Chicken</th>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+        <td className="prices-price">12.99</td>
+      </tr>
+    </table>
+  );
+}
